ac0f31762e362b455ad045f4815759402f95b30d
regat007 <regat007@gmail.com>
2019-07-01 11:52:55 +0100

    update2

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -6,12 +6,13 @@ object Prompt {
   def ask(message: String) = StdIn.readLine(message)
 }
 
-class Person(name : String) {
+class Person(name : String, age : Int) {
+  private val years : String = if(age > 1) "years" else "year"
   def speak() : String = {
     if (name == "adam") {
       s"You don't get a hello!"
     } else {
-      s"Hello $name"
+      s"Hello $name you are $age $years old"
     }
   }
 }
@@ -20,11 +21,25 @@ class Person(name : String) {
 object GreeterApplication extends App{
 
   val name = Prompt.ask("What is your name? ")
-  val person = new Person(name)
+  val age = Prompt.ask("How old are you? ")
+  val person = new Person(name,age.toInt)
   println(person.speak())
 
+abstract class BankAccount(accountNumber : String, balance: double){
 
+  def withdraw(amount : Double) : BankAccount
+  def deposit(amount : Double) : BankAccount
+}
 
+  final class SavingsAccount(accountNumber : String, balance: Double) extends BankAccount(accountNumber){
+    override def withdraw(amount: Double): BankAccount = {
+      0.00
+    }
+
+    override def deposit(amount: Double): BankAccount = {
+      0.00
+    }
+  }
 
 
 
