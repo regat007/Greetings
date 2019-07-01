d93b28c57739c639ad04f84a3b28fea8b4d1ec26
regat007 <regat007@gmail.com>
2019-07-01 13:28:58 +0100

    update3

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -18,30 +18,47 @@ class Person(name : String, age : Int) {
 }
 
 
-object GreeterApplication extends App{
+object GreeterApplication extends App {
 
   val name = Prompt.ask("What is your name? ")
   val age = Prompt.ask("How old are you? ")
-  val person = new Person(name,age.toInt)
+  val person = new Person(name, age.toInt)
   println(person.speak())
+}
 
-abstract class BankAccount(accountNumber : String, balance: double){
+abstract class BankAccount(accountNumber : String, balance: Double){
 
   def withdraw(amount : Double) : BankAccount
   def deposit(amount : Double) : BankAccount
 }
 
-  final class SavingsAccount(accountNumber : String, balance: Double) extends BankAccount(accountNumber){
-    override def withdraw(amount: Double): BankAccount = {
-      0.00
-    }
 
-    override def deposit(amount: Double): BankAccount = {
-      0.00
+final class SavingsAccount(accountNumber: String,balance : Double) extends BankAccount(accountNumber,balance){
+  override def withdraw(amount: Double): BankAccount = {
+    if ((balance - amount) < 0) {
+      println(s"You have insuffient funds")
+      this
+    } else {
+      val deducted = balance - amount
+      println(s"Balance after deducted: $deducted")
+      new SavingsAccount(accountNumber,deducted)
     }
   }
 
+  override def deposit(amount: Double): BankAccount = {
+    new SavingsAccount(accountNumber,balance+ amount)
+  }
 
+final class CashISASavingsAccount(accountNumber: String,balance : Double) extends BankAccount(accountNumber,balance){
+  override def withdraw(amount: Double): BankAccount = {
+    println(s"You can't withdraw yet, your money is locked away for 3 years!")
+    this
+  }
+
+  override def deposit(amount: Double): BankAccount = {
+    new CashISASavingsAccount(accountNumber,balance + amount)
+  }
+}
 
 
 
