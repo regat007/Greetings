8b7f6db28fe4ccda77a9f8cd3d689bd9c59a95dc
regat007 <regat007@gmail.com>
2019-07-01 11:32:35 +0100

    update

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -1,65 +1,36 @@
 package app
 
-import app.GreeterApplication.number
-
 import scala.io.StdIn
+
+object Prompt {
+  def ask(message: String) = StdIn.readLine(message)
+}
+
+class Person(name : String) {
+  def speak() : String = {
+    if (name == "adam") {
+      s"You don't get a hello!"
+    } else {
+      s"Hello $name"
+    }
+  }
+}
+
 
 object GreeterApplication extends App{
 
-  /*def greet(name : String) : Unit = {
-    if (name == "adam"){
-      println(s"You dont get a hello")
-    } else{
-      println(s"Hello $name")
-    }
-  }
-
-  val name = StdIn.readLine("What is your name? ")
-
-  greet(name)*/
+  val name = Prompt.ask("What is your name? ")
+  val person = new Person(name)
+  println(person.speak())
 
 
 
-  /*for (i <- 2 until number.toInt){
-    if ((number.toInt)%2 == 0){
-      prime = false
-    }
-
-
-  }
-
-  if (prime == true){
-    println(number + " is prime")
-  }else{
-    println(number + " is not prime")
-  }*/
-
-  def primeChecker(number : String) : Unit = {
-    var prime = true
 
 
 
-    for (i <- 2 until number.toInt){
-      println(i)
-      if ((number.toInt)%i == 0){
-        prime = false
-      }
-
-
-    }
-
-    if (prime == true){
-      println(number + " is prime")
-    }else{
-      println(number + " is not prime")
-    }
-  }
 
 
 
-  val number = StdIn.readLine("Enter number:")
-
-  primeChecker(number)
 
 
 
